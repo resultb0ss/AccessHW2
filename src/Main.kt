422cd75c2aa433b8a79b96caf8b31d8b2c2ed115
Admin <reteamboss@gmail.com>
2024-08-26 20:27:13 +0300

    Задание 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
-fun main() {
-    println("Hello World!")
+fun main(){
+
+    var array = arrayOf(
+        arrayOf(1,2,3,4),
+        arrayOf(5,6,7,8),
+        arrayOf(9,10,11,12),
+        arrayOf(13,14,15,16)
+    )
+
+    arrayCheckNum(array)
+
+}
+
+fun arrayCheckNum(array: Array<Array<Int>>){
+    for (row in array){
+        for (i in row){
+            if (i % 2 == 0){
+                println(i)
+            }
+        }
+    }
 }
